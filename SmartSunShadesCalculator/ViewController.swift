cfd8fe34229c8f81c32cf19da125614cab62c970
Ramdeen, Rashaad <rramdeen@national.aaa.com>
2016-05-03 16:41:36 -0400

    Initial Commit

---
+++ SmartSunShadesCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartSunShadesCalculator
+//
+//  Created by Ramdeen, Rashaad on 5/3/16.
+//  Copyright © 2016 Ramdeen, Rashaad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
